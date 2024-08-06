fdc303ec51177969f02c32b4f349a71bc1bae4da
GurkiratSingh111 <sgurkirat868@gmail.com>
2024-08-06 12:46:41 -0700

    Created a Circle component

--- client/src/components/BlogCard.tsx
+++ client/src/components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import Avatar from "./Avatar"
+import Circle from "./Circle"
 
 interface BlogCardProps{
     authorName: string,
@@ -14,19 +15,23 @@ const BlogCard = ({authorName, title, content, publishedDate}: BlogCardProps) =>
     
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col m-2 border-b border-slate-200 pb-2 ">
         <div className="flex">
             <Avatar authorName={authorName}/>
-            <div className="text-slate-500">{authorName}. {publishedDate}</div>
+            <div className="text-slate-500 flex items-center ml-2 font-semibold">{authorName}</div>
+            <div className="flex flex-col justify-center m-2">
+                <Circle/>
+            </div>
+            
+            <div className="text-slate-500 flex items-center ml-2">{publishedDate}</div>
         </div>
         <div className="font-bold text-2xl">{title}</div>
-        <div className="text-lg">
+        <div className="text-lg mt-2">
             {content.slice(0,100) + "..."}
         </div>
-        <div className="mt-8 text-slate-700">
+        <div className="mt-8 text-slate-700 pb-2">
             {`${Math.floor(content.length/100)} minutes read`}
         </div>
-        <div className="bg-slate-200 h-1 w-4/6"  />
       
     </div>
   )
